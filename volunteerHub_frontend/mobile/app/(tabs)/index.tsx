beb8d9376781dae88b053a11c64ccdb71f82fe8b
Andreea Arseanu <andreea.arseanu@student.upt.ro>
2026-01-26 01:18:52 +0200

    add logout button for testing purposes

--- volunteerHub_frontend/mobile/app/(tabs)/index.tsx
+++ volunteerHub_frontend/mobile/app/(tabs)/index.tsx
@@ -1,9 +1,22 @@
-import { StyleSheet, Text, View } from "react-native";
+import { logout } from "@/src/store/auth.store";
+import { useRouter } from "expo-router";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen() {
+  const router = useRouter();
+
+  async function handleLogout() {
+    await logout();
+    router.replace("/(auth)/login");
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Home</Text>
+
+      <Pressable style={styles.logoutBtn} onPress={handleLogout}>
+        <Text style={styles.logoutBtnText}>Logout</Text>
+      </Pressable>
     </View>
   );
 }
@@ -17,5 +30,17 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: "bold",
+  },
+  logoutBtn: {
+    marginTop: 24,
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    backgroundColor: "#C1121F",
+    borderRadius: 8,
+  },
+  logoutBtnText: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    fontWeight: "600",
   },
 });
