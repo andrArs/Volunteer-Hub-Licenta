d738e171202f3a0f22eaaad7c0325c7e8bcba081
Andreea Arseanu <andreea.arseanu@student.upt.ro>
2026-03-17 21:06:53 +0200

    fix: hide past events on view events screen

--- volunteerHub_frontend/mobile/app/(tabs)/events.view.tsx
+++ volunteerHub_frontend/mobile/app/(tabs)/events.view.tsx
@@ -82,7 +82,13 @@ export default function AllEventsScreen() {
       setErr(null);
       setLoading(true);
       const data = await getAllEvents();
-      setEvents(data ?? []);
+      const now = new Date();
+        const futureEvents = (data || []).filter(ev => {
+          const eventDate = new Date(ev.startDateTime);
+          return eventDate >= now;
+        });
+
+        setEvents(futureEvents);
     } catch (e: any) {
       setErr(e?.message ?? "Failed to load events.");
     } finally {
